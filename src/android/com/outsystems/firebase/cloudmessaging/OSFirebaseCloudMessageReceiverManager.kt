5f856f959248ba96ed64e605b21b595c3923f0e0
DiogoMadeiraOS <144679900+DiogoMadeiraOS@users.noreply.github.com>
2023-09-17 18:05:44 +0100

    Create OSFirebaseCloudMessageReceiverManager.kt

---
+++ src/android/com/outsystems/firebase/cloudmessaging/OSFirebaseCloudMessageReceiverManager.kt
@@ -0,0 +1,33 @@
+package org.apache.cordova.firebase
+
+import android.util.Log
+import com.google.firebase.messaging.RemoteMessage
+import java.util.ArrayList
+import java.util.List
+
+object FirebasePluginMessageReceiverManager {
+    private const val TAG = "FirebasePlugin"
+    private val receivers: List<FirebasePluginMessageReceiver> =
+        ArrayList<FirebasePluginMessageReceiver>()
+
+    fun register(receiver: FirebasePluginMessageReceiver?) {
+        Log.d(TAG, "FirebasePluginMessageReceiverManager register called")
+        receivers.add(receiver)
+    }
+
+    fun onMessageReceived(remoteMessage: RemoteMessage?): Boolean {
+        Log.d(TAG, "FirebasePluginMessageReceiverManager onMessageReceived called")
+        var handled = false
+        for (receiver in receivers) {
+            val wasHandled: Boolean = receiver.onMessageReceived(remoteMessage)
+            if (wasHandled) {
+                handled = true
+            }
+        }
+        Log.d(
+            TAG,
+            "FirebasePluginMessageReceiverManager onMessageReceived handled: " + if (handled) "true" else "false"
+        )
+        return handled
+    }
+}
