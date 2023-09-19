c0cac6cfa95663b3c9b578311f0c8b1cef417a02
Diogo Madeira <madeiradiogo@outlook.pt>
2023-09-19 23:41:38 +0100

    Update FirebaseApplication.kt

--- src/android/com/outsystems/firebase/cloudmessaging/FirebaseApplication.kt
+++ src/android/com/outsystems/firebase/cloudmessaging/FirebaseApplication.kt
@@ -1,7 +1,6 @@
 package com.outsystems.firebase.cloudmessaging;
 
 import android.app.Application
-import com.outsystems.firebase.cloudmessaging.AppForegroundStateManager
 
 class FirebaseApplication : Application() {
 
@@ -13,23 +12,5 @@ class FirebaseApplication : Application() {
         // Register the AppLifecycleObserver
         registerActivityLifecycleCallbacks(FirebaseLifecycleApplication())
     }
-
-    override fun onPause() {
-        foregroundCount--
-        if (foregroundCount == 0) {
-            // App goes from foreground to background
-            // You can trigger actions here
-            AppForegroundStateManager.setAppInForeground(false)
-        }
-    }
-    override fun onResume() {
-        if (foregroundCount == 0) {
-            // App goes from background to foreground
-            // You can trigger actions here
-            AppForegroundStateManager.setAppInForeground(true)
-        }
-        foregroundCount++
-    }
-
 
 }
