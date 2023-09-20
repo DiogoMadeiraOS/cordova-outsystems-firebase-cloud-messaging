81481e1348211e1f3da24584e8753e40dfad650c
Diogo Madeira <madeiradiogo@outlook.pt>
2023-09-20 18:33:47 +0100

    JSONARRAY

--- src/android/com/outsystems/firebase/cloudmessaging/OnNotificationOpenReceiver.kt
+++ src/android/com/outsystems/firebase/cloudmessaging/OnNotificationOpenReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import org.json.JSONArray
 
 class OnNotificationOpenReceiver : BroadcastReceiver() {
 
